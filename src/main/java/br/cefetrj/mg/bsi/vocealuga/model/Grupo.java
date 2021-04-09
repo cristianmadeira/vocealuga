653d51a3845eee2dc4f76f556d3b736ef4143a22
Cristian Madeira de Souza Pereira <cristian.pereira@aluno.cefet-rj.br>
2021-04-09 19:44:16 -0300

    Add timestamp for grupo model

--- src/main/java/br/cefetrj/mg/bsi/vocealuga/model/Grupo.java
+++ src/main/java/br/cefetrj/mg/bsi/vocealuga/model/Grupo.java
@@ -1,14 +1,23 @@
 package br.cefetrj.mg.bsi.vocealuga.model;
+import java.time.LocalDateTime;
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.validation.constraints.Size;
 
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.hibernate.annotations.Where;
+
 
 
 @Entity
-
+@SQLDelete(sql = "UPDATE grupo set  deleted_at = current_timestamp() WHERE id = ?")
+@Where(clause = "deleted_at is null")
 public class Grupo {
 	
 	@Id
@@ -18,6 +27,17 @@ public class Grupo {
 	@Size(min = 5, max = 50, message =  "O nome deve ter entre {min} e {max} caracteres.")
 	private String nome;
 
+	@Column(updatable = false)
+	@CreationTimestamp
+	private LocalDateTime createdAt;
+
+	@Column(insertable = false)
+	@UpdateTimestamp
+	private LocalDateTime updatedAt;
+
+	private LocalDateTime deletedAt;
+
+	
 	public Integer getId() {
 		return id;
 	}
@@ -29,5 +49,26 @@ public class Grupo {
 	}
 	public void setNome(String nome) {
 		this.nome = nome;
+	}
+
+	
+	public LocalDateTime getCreatedAt() {
+		return createdAt;
+	}
+	public void setCreatedAt(LocalDateTime createdAt) {
+		this.createdAt = createdAt;
+	}
+
+	public LocalDateTime getUpdatedAt() {
+		return updatedAt;
+	}
+	public void setUpdatedAt(LocalDateTime updatedAt) {
+		this.updatedAt = updatedAt;
+	}
+	public LocalDateTime getDeletedAt() {
+		return deletedAt;
+	}
+	public void setDeletedAt(LocalDateTime deletedAt) {
+		this.deletedAt = deletedAt;
 	}
 }
