772c315d4f9e4408c45df92378a57ee0f4145846
Cristian Madeira de Souza Pereira <cristian.pereira@aluno.cefet-rj.br>
2021-04-09 23:16:25 +0000

    Add timestamp for Cargo Model

--- src/main/java/br/cefetrj/mg/bsi/vocealuga/model/Cargo.java
+++ src/main/java/br/cefetrj/mg/bsi/vocealuga/model/Cargo.java
@@ -1,20 +1,40 @@
 package br.cefetrj.mg.bsi.vocealuga.model;
 
+import java.time.LocalDateTime;
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.validation.constraints.Size;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.hibernate.annotations.Where;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
+@SQLDelete(sql = "UPDATE cargo SET deleted_at = current_timestamp() WHERE id = ?")
+@Where(clause = "deleted_at is null")
 public class Cargo {
 
     @Id
     @GeneratedValue(strategy=GenerationType.AUTO)
     private Integer id;
 
+    @Column(nullable = false, length = 50)
     @Size(min = 5, max = 50, message = "O nome deve ter entre {min} e {max} caracteres. ")
     private String nome;
+
+    @Column(updatable = false)
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @Column(insertable = false)
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+    
+    private LocalDateTime deletedAt;
 
     public Integer getId() {
         return id;
@@ -28,5 +48,24 @@ public class Cargo {
     public void setNome(String nome) {
         this.nome = nome;
     }
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+    public void setUpdatedAt(LocalDateTime updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+    public LocalDateTime getDeletedAt() {
+        return deletedAt;
+    }
+    public void setDeleteAt(LocalDateTime deletedAt) {
+        this.deletedAt = deletedAt;
+    }
+
 
 }
