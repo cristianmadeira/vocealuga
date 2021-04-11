13ea9cbc9b531af7c333b7bbbb8549b5f77ee21c
Cristian Madeira de Souza Pereira <cristian.pereira@aluno.cefet-rj.br>
2021-04-11 17:25:07 +0000

    Create Funcionario Repository.

---
+++ src/main/java/br/cefetrj/mg/bsi/vocealuga/repository/FuncionarioRepository.java
@@ -0,0 +1,8 @@
+package br.cefetrj.mg.bsi.vocealuga.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import br.cefetrj.mg.bsi.vocealuga.model.Funcionario;
+public interface FuncionarioRepository extends CrudRepository<Funcionario, Integer> {
+ 
+}
