320d9d5b43770e7e446701ec089c33efbb6a9c0a
Cristian Madeira de Souza Pereira <cristian.pereira@aluno.cefet-rj.br>
2021-04-12 08:57:57 -0300

    Configure Debug

--- start.sh
+++ start.sh
@@ -7,7 +7,7 @@ echo 'Updating source list ...'
 apt update -y 
 apt install dos2unix
 dos2unix mvnw
-mvn spring-boot:run -Dspring-boot.run.jvmArguments="-Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=y,address=5005"
+mvn spring-boot:run -Dspring-boot.run.jvmArguments="-Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=5005"
 #./mvnw spring-boot:run -Dspring-boot.run.jvmArguments="agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5005"
 #./mvnwDebug spring-boot:run
 #./mvnw spring-boot:run -Ddwp:server=y,transport=dt_socket,address=5000,suspend=n
