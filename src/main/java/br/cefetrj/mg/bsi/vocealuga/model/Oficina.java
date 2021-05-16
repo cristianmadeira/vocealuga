8ba51b7937e27fde41a85208ab103fab37300594
MauricioBM <mauriciobmrjt@gmail.com>
2021-05-16 16:10:20 -0300

    createdAt, updatedAt e deletedAt adicionados.

--- src/main/java/br/cefetrj/mg/bsi/vocealuga/model/Oficina.java
+++ src/main/java/br/cefetrj/mg/bsi/vocealuga/model/Oficina.java
@@ -1,5 +1,8 @@
 package br.cefetrj.mg.bsi.vocealuga.model;
 
+import java.time.LocalDateTime;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -8,22 +11,24 @@ import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
 
+import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.UpdateTimestamp;
 import org.hibernate.annotations.Where;
-
 
 @Entity
 @SQLDelete(sql = "UPDATE oficina SET deleted_at = current_timestamp() WHERE id = ?")
 @Where(clause = "deleted_at is null")
 public class Oficina {
+
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
-    
+
     @NotBlank(message = "O Nome não pode estar em branco.")
     @Size(min = 5, max = 50, message = "O nome deve ter entre {min} e {max} caracteres.")
     private String nome;
-    
+
     @Email(message = "O E-mail deve ser válido.")
     @NotBlank(message = "O E-mail não pode ser em branco.")
     private String email;
@@ -31,6 +36,16 @@ public class Oficina {
     @NotBlank(message = "O telefone não pode ser em branco.")
     @Size(min = 10, max = 11, message = "O telefone deve ter entre {min} e {max} caracteres.")
     private String telefone;
+
+    @Column(updatable = false)
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @Column(insertable = false)
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+
+    private LocalDateTime deletedAt;
 
     public Integer getId() {
         return this.id;
@@ -63,13 +78,29 @@ public class Oficina {
     public void setTelefone(String telefone) {
         this.telefone = telefone;
     }
-    
 
-    
-    
-    
-    
-    
+    public LocalDateTime getCreatedAt() {
+        return this.createdAt;
+    }
 
-    
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return this.updatedAt;
+    }
+
+    public void setUpdatedAt(LocalDateTime updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    public LocalDateTime getDeletedAt() {
+        return this.deletedAt;
+    }
+
+    public void setDeletedAt(LocalDateTime deletedAt) {
+        this.deletedAt = deletedAt;
+    }
+
 }
