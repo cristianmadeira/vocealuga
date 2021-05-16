01f9573a0dd3457396dd50542c707dcff1ea77dd
Cristian Madeira de Souza Pereira <cristian.pereira@aluno.cefet-rj.br>
2021-05-16 12:06:50 +0000

    Add findByCpf method

--- src/main/java/br/cefetrj/mg/bsi/vocealuga/repository/ClienteRepository.java
+++ src/main/java/br/cefetrj/mg/bsi/vocealuga/repository/ClienteRepository.java
@@ -1,9 +1,12 @@
 package br.cefetrj.mg.bsi.vocealuga.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.repository.CrudRepository;
 
 import br.cefetrj.mg.bsi.vocealuga.model.Cliente;
 
 public interface ClienteRepository  extends CrudRepository<Cliente,Integer>{
     
+    public Optional<Cliente> findByCpf(String cpf);
 }
