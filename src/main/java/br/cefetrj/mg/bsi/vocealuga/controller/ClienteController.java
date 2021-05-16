6e4431ea3ade6c10745958488bcf034f4d227872
Cristian Madeira de Souza Pereira <cristian.pereira@aluno.cefet-rj.br>
2021-05-16 12:03:39 +0000

    Add account method

--- src/main/java/br/cefetrj/mg/bsi/vocealuga/controller/ClienteController.java
+++ src/main/java/br/cefetrj/mg/bsi/vocealuga/controller/ClienteController.java
@@ -1,19 +1,41 @@
 package br.cefetrj.mg.bsi.vocealuga.controller;
-
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import br.cefetrj.mg.bsi.vocealuga.model.Cliente;
+import br.cefetrj.mg.bsi.vocealuga.model.User;
+import br.cefetrj.mg.bsi.vocealuga.repository.ClienteRepository;
+
 @Controller
-@RequestMapping("clientes")
+@RequestMapping("/clientes")
 public class ClienteController {
     
 
+    @Autowired
+    private ClienteRepository clienteRepository;
     
-    @GetMapping("{id}/pedidos")
-    public ModelAndView pedidos(ModelAndView modelAndView){
-        Cliente = 
-        return modelAndView;
+    
+    
+    @GetMapping("/account")
+    public ModelAndView account(
+        Authentication authentication,
+        ModelAndView modelAndView
+        ){
+            Cliente cliente = new Cliente();
+            Object principal = authentication.getPrincipal();
+            if( principal instanceof User){
+                User u = (User) principal;
+                cliente = this.clienteRepository.findByCpf(u.getCpf()).get();
+                
+            }
+            modelAndView.setViewName("clientes/account");
+            modelAndView.addObject("cliente", cliente);
+            modelAndView.addObject("buttonName", "Editar");
+            
+            return modelAndView;
     }
 }
