bc51f42ad1223fbfcab381aa2dc42c678ad95aaf
Cristian Madeira de Souza Pereira <cristian.pereira@aluno.cefet-rj.br>
2021-05-16 12:07:53 +0000

    Add findByVeiculoDisponivel method

--- src/main/java/br/cefetrj/mg/bsi/vocealuga/repository/VeiculoRepository.java
+++ src/main/java/br/cefetrj/mg/bsi/vocealuga/repository/VeiculoRepository.java
@@ -1,9 +1,14 @@
 package br.cefetrj.mg.bsi.vocealuga.repository;
 
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import br.cefetrj.mg.bsi.vocealuga.model.Veiculo;
 
 public interface VeiculoRepository extends CrudRepository<Veiculo, Integer> {
     
+    @Query("SELECT v FROM Veiculo v WHERE v.disponivel = 1 AND v.deletedAt is null")
+    public List<Veiculo> findByVeiculoDisponivel();
 }
