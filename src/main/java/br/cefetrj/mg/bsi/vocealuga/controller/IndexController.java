7b55c2469920ac1b2262b2a7446b14786ae074c7
Cristian Madeira de Souza Pereira <cristian.pereira@aluno.cefet-rj.br>
2021-05-16 12:04:28 +0000

    Search All ok Cars

--- src/main/java/br/cefetrj/mg/bsi/vocealuga/controller/IndexController.java
+++ src/main/java/br/cefetrj/mg/bsi/vocealuga/controller/IndexController.java
@@ -1,16 +1,27 @@
 package br.cefetrj.mg.bsi.vocealuga.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import br.cefetrj.mg.bsi.vocealuga.repository.VeiculoRepository;
 
 @Controller
 @RequestMapping("/")
 public class IndexController {
 	
+	private VeiculoRepository veiculoRepository;
+	@Autowired
+	public IndexController(VeiculoRepository veiculoRepository){
+		this.veiculoRepository = veiculoRepository;
+	}
 	@GetMapping
-	public String index() {
-		return "index";
+	public ModelAndView index(ModelAndView modelAndView) {
+		modelAndView.setViewName("index");
+		modelAndView.addObject("veiculos",this.veiculoRepository.findByVeiculoDisponivel());
+		return modelAndView;
 	}
 
 }
